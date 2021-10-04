7119274813cceadc3f0d96577367105b89972b00
kotiq <courier.yeti@gmail.com>
2021-10-04 17:52:41 +0600

    vromfs: initial unpack

--- src/vromfs/constructor.py
+++ src/vromfs/constructor.py
@@ -1,8 +1,11 @@
+import io
 from enum import Enum
 from pathlib import Path
 import typing as t
 import construct as ct
+import zstandard as zstd
 from construct import this
+from .reader import RangedReader
 
 
 class HeaderType(Enum):
@@ -41,10 +44,16 @@ class BinContainerInfo(t.NamedTuple):
     """Размер неупакованного образа"""
 
     packed_size: t.Optional[int]
-    """Размер упакованного образа, None для PLAIN"""
+    """Размер упакованного образа. None для PackType.PLAIN"""
 
     hash: t.Optional[bytes]
-    """MD5 хэш распакованного образа, None для ZSTD_OBFS_NOCHECK"""
+    """MD5 хэш распакованного образа. None для PackType.ZSTD_OBFS_NOCHECK"""
+
+    version: t.Optional[t.Tuple[int, int, int, int]]
+    """Версия контейнера. None для HeaderType.VRFS"""
+
+    platform: PlatformType
+    """Целевая платформа"""
 
 
 class UnpackResult(t.NamedTuple):
@@ -75,9 +84,8 @@ HeaderTypeCon = enum(ct.Bytes(4), HeaderType)
 PlatformTypeCon = enum(ct.Bytes(4), PlatformType)
 PackTypeCon = enum(ct.BitsInteger(6), PackType)
 
-
 BinExtHeader = ct.Struct(
-    'size' / ct.Rebuild(ct.Int16ul, lambda ctx: sum(map(lambda sc: sc.sizeof(),  BinExtHeader.subcons))),
+    'size' / ct.Rebuild(ct.Int16ul, lambda ctx: sum(map(lambda sc: sc.sizeof(), BinExtHeader.subcons))),
     'flags' / ct.Int16ul,
     'version' / VersionCon,
 )
@@ -93,6 +101,85 @@ BinHeader = ct.Struct(
     )),
 )
 
+BinContainer = ct.Struct(
+    'header' / BinHeader,
+    'ext_header' / ct.If(lambda ctx: ctx.header.type is HeaderType.VRFX, BinExtHeader),
+    'offset' / ct.Tell,
+    ct.Seek(lambda ctx: ctx.header.size
+            if ctx.header.packed.type is PackType.PLAIN
+            else ctx.header.packed.size, io.SEEK_CUR),
+    'hash' / ct.If(lambda ctx: ctx.header.packed.type is not PackType.ZSTD_OBFS_NOCHECK, ct.Bytes(16)),
+    'tail' / ct.GreedyBytes,
+    ct.Check(lambda ctx: len(ctx.tail) in (0, 0x100)),
+)
+
+
+def decompress(istream: t.BinaryIO) -> t.BinaryIO:
+    """
+    Декомпрессия сжатого образа
+
+    :param istream: входной поток сжатого образа
+    :return: выходной поток несжатого образа
+    """
+
+    dctx = zstd.ZstdDecompressor()
+    return dctx.stream_reader(istream)
+
+
+def compress(istream: t.BinaryIO) -> t.BinaryIO:
+    """
+    Сжатие образа
+
+    :param istream: входной поток несжатого образа
+    :return: выходной поток сжатого образа
+    """
+
+    raise NotImplementedError
+
+
+def inplace_xor(buffer: bytearray, from_: int, sz: int, key: t.Iterable[int]) -> None:
+    it = iter(key)
+    for i in range(from_, from_ + sz):
+        buffer[i] ^= next(it)
+
+
+obfs_ks: t.Sequence[bytes] = [bytes.fromhex(s) for s in ('55aa55aa', '0ff00ff0', '55aa55aa', '48124812')]
+
+
+# todo: перестроить как ленивый FileReader
+def deobfuscate(istream: t.BinaryIO, sz: int) -> t.BinaryIO:
+    """
+    Восстановление заголовка и окончания
+
+    :param istream: входной поток сжатого образа
+    :param sz: размер сжатого образа
+    :return: выходной поток сжатого образа с измененными частями
+    """
+
+    bs = istream.read()
+    buffer = bytearray(bs)
+    key_sz = sum(map(len, obfs_ks))
+    if sz >= key_sz:
+        pos = 0
+        inplace_xor(buffer, pos, key_sz, b''.join(obfs_ks))
+        if sz >= 2 * key_sz:
+            pos = (sz & 0x03ff_fffc) - key_sz
+            inplace_xor(buffer, pos, key_sz, b''.join(reversed(obfs_ks)))
+
+    return io.BytesIO(buffer)
+
+
+def obfuscate(istream: t.BinaryIO, sz: int) -> t.BinaryIO:
+    """
+    Сокрытие заголовка и окончания
+
+    :param istream: входной поток сжатого образа с измененными частями
+    :param sz: размер сжатого образа
+    :return: выходной поток сжатого образа
+    """
+
+    return deobfuscate(istream, sz)
+
 
 def unpack(istream: t.BinaryIO) -> UnpackResult:
     """
@@ -102,7 +189,38 @@ def unpack(istream: t.BinaryIO) -> UnpackResult:
     :return: результат распаковки
     """
 
-    raise NotImplementedError
+    try:
+        bin_container = BinContainer.parse_stream(istream)
+    except ct.ConstructError as e:
+        raise UnpackError(e)
+    else:
+        pack_type: PackType = bin_container.header.packed.type
+
+        if pack_type not in (PackType.PLAIN, PackType.ZSTD_OBFS, pack_type.ZSTD_OBFS_NOCHECK):
+            raise UnpackError("Unknown pack_type: {}".format(pack_type))
+
+        offset = bin_container.offset
+        size = bin_container.header.size
+        platform = bin_container.header.platform
+
+        header_type: HeaderType = bin_container.header.type
+        version = None if header_type is HeaderType.VRFS else bin_container.ext_header.version
+        packed_size = None if pack_type is PackType.PLAIN else bin_container.header.packed.size
+        hash_ = None if pack_type is PackType.ZSTD_OBFS_NOCHECK else bin_container.hash
+        ostream = (RangedReader(istream, offset, offset+size)
+                   if pack_type is PackType.PLAIN
+                   else decompress(deobfuscate(RangedReader(istream, offset, offset+packed_size), packed_size)))
+
+        return UnpackResult(
+            ostream=ostream,
+            info=BinContainerInfo(
+                unpacked_size=size,
+                packed_size=packed_size,
+                hash=hash_,
+                version=version,
+                platform=platform
+            )
+        )
 
 
 def pack(istream: t.BinaryIO,
@@ -111,7 +229,7 @@ def pack(istream: t.BinaryIO,
     Упаковка образа в bin контейнер: image -> image.bin
 
     :param istream: входной поток образа
-    :param container_type: тип заголовка
+    :param type: тип заголовка
     :param platform: целевая платформа
     :param compress: сжимать ли образ
     :param check: есть ли контрольная сумма
---
+++ tests/test_vromfs/test_constructor/test_api/conftest.py
@@ -0,0 +1,34 @@
+import io
+import typing as t
+import pytest
+from vromfs.constructor import UnpackResult, BinContainerInfo, PlatformType
+
+
+@pytest.fixture(scope='module')
+def vrfs_pc_plain_unpack_result(image: bytes, hash_: bytes) -> UnpackResult:
+    return UnpackResult(
+        ostream=io.BytesIO(image),
+        info=BinContainerInfo(
+            unpacked_size=len(image),
+            packed_size=None,
+            hash=hash_,
+            version=None,
+            platform=PlatformType.PC,
+        )
+    )
+
+
+@pytest.fixture(scope='module')
+def vrfx_pc_zstd_obfs_unpack_result(image: bytes, obfuscated_compressed_image: bytes, version: t.Tuple[int, int, int, int],
+                                 hash_: bytes) -> UnpackResult:
+    return UnpackResult(
+        ostream=io.BytesIO(image),
+        info=BinContainerInfo(
+            unpacked_size=len(image),
+            packed_size=len(obfuscated_compressed_image),
+            hash=hash_,
+            version=version,
+            platform=PlatformType.PC
+        )
+    )
+
